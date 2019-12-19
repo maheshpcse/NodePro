7af4f92d651ee63cfde9861ee5dc98c9bc5ebc9b
Mahesh <maheshmahi1599@gmail.com>
2019-12-19 21:56:46 +0530

    changes

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -10,12 +10,12 @@ module.exports.userLogin = (req, res, next) => {
     console.log("request fields are:", req.body);
     
     if (!req.body.username || !req.body.password) {
-        return res.status(404).json({
+        return res.status(200).json({
             success: false,
-            statusCode: 404,
+            statusCode: 204,
             message: 'Required fields are empty, Please check once',
             data: null
-        })
+        });
     }
 
     let wherecond = `username='${req.body.username}' AND password='${req.body.password}'`;
@@ -24,7 +24,12 @@ module.exports.userLogin = (req, res, next) => {
         console.log("response is:", result);
         if(result == '' || result == null) {
             console.log("Invalid username or password");
-            return res.status(404).send(result);
+            return res.status(200).json({
+                success: false,
+                statusCode: 404,
+                message: 'Invalid username or password',
+                data: result
+            });
         }
         console.log("Login successful");
         var token = jwt.sign({
@@ -43,6 +48,11 @@ module.exports.userLogin = (req, res, next) => {
         });
     }).catch(err => {
         console.log("Error while login", err);
-        res.status(500).send(err);
+        res.status(200).json({
+            success: false,
+            statusCode: 500,
+            message: 'Error while login',
+            data: err
+        });
     })
 }
--- src/seeds/01_users.js
+++ src/seeds/01_users.js
@@ -5,36 +5,51 @@ exports.seed = function (knex) {
       // Inserts seed entries
       return knex('users').insert([{
           "user_id": "1",
+          "username": "nigel",
           "email": "nigel@email.com",
-          "password": "dorwssap",
+          "password": "password",
+          "phonenumber": "9876512340",
+          "role": "user",
           "created_at": "2019-12-16 10:19:46",
           "updated_at": "2019-12-16 10:19:46"
         },
         {
           "user_id": "2",
+          "username": "nakaz",
           "email": "nakaz@email.com",
           "password": "password1",
+          "phonenumber": "7661876696",
+          "role": "manager",
           "created_at": "2019-12-16 10:19:46",
           "updated_at": "2019-12-16 10:19:46"
         },
         {
           "user_id": "3",
+          "username": "jaywon",
           "email": "jaywon@email.com",
-          "password": "password123",
+          "password": "password12",
+          "phonenumber": "9949393483",
+          "role": "user",
           "created_at": "2019-12-16 10:19:46",
           "updated_at": "2019-12-16 10:19:46"
         },
         {
           "user_id": "8",
-          "email": "smith123@email.com",
-          "password": "password1234",
+          "username": "smith",
+          "email": "smith@email.com",
+          "password": "password123",
+          "phonenumber": "1234598760",
+          "role": "manager",
           "created_at": "2019-12-17 18:03:14",
           "updated_at": "2019-12-17 18:03:14"
         },
         {
           "user_id": "10",
-          "email": "john123@email.com",
-          "password": "password123456",
+          "username": "john",
+          "email": "john@email.com",
+          "password": "password1234",
+          "phonenumber": "8332005225",
+          "role": "admin",
           "created_at": "2019-12-17 18:19:50",
           "updated_at": "2019-12-17 18:19:50"
         }
